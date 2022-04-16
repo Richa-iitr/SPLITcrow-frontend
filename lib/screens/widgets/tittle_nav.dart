7e425990c91dd2b4f558ba69f6456a96661b897f
Richa-iitr <richa31102@gmail.com>
2022-04-16 12:32:55 +0530

    menu, add expense

--- lib/screens/widgets/tittle_nav.dart
+++ lib/screens/widgets/tittle_nav.dart
@@ -39,7 +39,64 @@ class TittleNav extends StatelessWidget {
             children: [
               
               RoundedButton(btn_name: 'Add Rs', press: () {
-
+                showDialog(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return AlertDialog(
+                            scrollable: true,
+                            title: Text('ADD EXPENSE'),
+                            content: Padding(
+                              padding: const EdgeInsets.all(5.0),
+                              child: Form(
+                                child: Column(
+                                  children: <Widget>[
+                                    TextFormField(
+                                      decoration: InputDecoration(
+                                        labelText: 'With you and ..',
+                                        icon:
+                                            Icon(Icons.social_distance_rounded),
+                                      ),
+                                    ),
+                                    TextFormField(
+                                      decoration: InputDecoration(
+                                        labelText: 'Description',
+                                        icon:
+                                            Icon(Icons.note),
+                                      ),
+                                    ),
+                                    TextFormField(
+                                      decoration: InputDecoration(
+                                        labelText: 'Amount',
+                                        icon:
+                                            Icon(Icons.money),
+                                      ),
+                                    ),
+                                    Text('  '),
+                                    Row(children: [
+                                    Text('Paid by '),
+                                    Text('and split '),],),
+                                  ],
+                                ),
+                              ),
+                            ),
+                            actions: [
+                              Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    RoundedButton(
+                                        btn_name: 'Submit', press: () {})
+                                  ]),
+                              Divider(color: Colors.black),
+                              Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    RoundedButton(
+                                        btn_name: 'UPI PAY', press: () {})
+                                  ]),
+                            ],
+                          );
+                        });
+      
               })
              
             ],
